b0f237a1dedd2f04cc763b86d40bedbd38db2a44
Aman Sharma <76823502+arcAman07@users.noreply.github.com>
2022-06-28 22:55:16 +0530

    Time: 49 ms (15.93%), Space: 17 MB (51.91%) - LeetHub

---
+++ 136-single-number/136-single-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        std::sort(nums.begin(),nums.end());
+        if (nums.size()>=3)
+        {
+            for(int i=0;i<nums.size()-2;i+=2)
+        {
+            if (nums[i] != nums[i+1])
+            {
+                return nums[i];
+            }
+        }
+        return nums[nums.size()-1];
+    }
+        return nums[0];
+        }
+        
+};
